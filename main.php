3de53313d22303c66fa3c5edc7badaa63c77d153
NordinTop <78902281+NordinTop@users.noreply.github.com>
2022-03-16 15:16:36 +0100

    buttons

--- main.php
+++ main.php
@@ -35,7 +35,7 @@
     
     <div class="container pt-5">
     </div>
-    <div class="nieuwbutton"><button type="button" class="btn btn-danger"><i class="fa-solid fa-bookmark"></i> Opslaan</button></div>
+    <div class="nieuwbutton"><button type="button" class="btn btn-success"><i class="fa-solid fa-bookmark"></i> Opslaan</button></div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <?php

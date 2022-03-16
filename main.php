0e191a3c53d896da679c89fa6e822da99fb05aa5
NordinTop <78902281+NordinTop@users.noreply.github.com>
2022-03-16 14:08:08 +0100

    redirects

--- main.php
+++ main.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <script src="https://kit.fontawesome.com/6f3d41182b.js" crossorigin="anonymous"></script>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -13,17 +14,17 @@
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/main.php">HOME</a>
+        <a class="navbar-brand" href="main.php">HOME</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/opgeslagen.php">OPGESLAGEN</a>
+              <a class="nav-link active" aria-current="page" href="opgeslagen.php">OPGESLAGEN</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/help.php">HELP</a>
+              <a class="nav-link" href="help.php">HELP</a>
             </li>
           </ul>
         </div>
@@ -34,11 +35,18 @@
     
     <div class="container pt-5">
     </div>
-    <div> <button>Add</button>
-    </div>
-    <div> <button>Vernieuwen</button>
+    <div class="nieuwbutton"><button type="button" class="btn btn-danger"><i class="fa-solid fa-bookmark"></i> Opslaan</button></div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <?php
+      $api_url = 'https://aventus-173566.aventusfactory.nl/Recepten/connect.php';
+      $data = json_decode( file_get_contents($api_url), true);
 
+      $gerecht = $data[0]['name'];
+      $recept = $data[0][''];
+      $ingredient = $data[0]['ingredients'];
+      $portie = $data[0]['portions'];
+      ?>
+      <div class="buttonrefresh"><button type="button" onClick="window.location.reload()" class="btn btn-danger"><i class="fa-solid fa-arrow-rotate-right"></i> Nieuw recept</button></div>
   </body>
 </html>

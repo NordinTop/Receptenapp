b0ab1e799d0cc2786d02f6c45f9395ffa7707aa6
NordinTop <78902281+NordinTop@users.noreply.github.com>
2022-03-17 10:31:05 +0100

    image/wrapper

--- main.php
+++ main.php
@@ -35,6 +35,13 @@
         </div>
       </div>
     </nav>
+    <div class="wrapper">
+  <div class="col-md-6">
+    <div class=" flex">
+      <div class="media-left"> </div>
+      <div class="media-body"> </div>
+    </div>
+  </div>
     <h1 class="h1_class">Recepten randomizer</h1>
     
     
@@ -43,6 +50,7 @@
     <div class="nieuwbutton"><button type="button" class="btn btn-success"><i class="fa-solid fa-bookmark"></i> Opslaan</button></div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
     <?php
       $api_url = 'https://aventus-173566.aventusfactory.nl/Recepten/connect.php';
       $data = json_decode( file_get_contents($api_url), true);

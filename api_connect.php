72abd468a8a995364b23345d122fe1a96a70bf56
tjeerd688 <78901275+tjeerd688@users.noreply.github.com>
2022-03-18 11:44:26 +0100

    Add files via upload
    de connect van de api die op aventus factory staat

---
+++ api_connect.php
@@ -0,0 +1,71 @@
+<?php
+   
+  function preprint_r( $arr ) {
+    echo '<pre>'; 
+    print_r( $arr );
+    echo '</pre>'; 
+  }  
+
+  
+  //verdindingsvariabelen database 
+$host = 'localhost';
+$db   = 'aventus-173566_database';
+$user = 'aventus-173566_student';
+$pass = '2A7%BiDgcbhuu2ql';
+$charset = 'utf8mb4';
+  
+  
+  //connection string en PDO settings
+  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+  $options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+  ];
+  
+  
+  //verbinding maken met database
+  try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+  } catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+  }   
+      
+// Check connection
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+} else 
+    
+    
+$stmt = $pdo->query ("SELECT * FROM RECEPT WHERE `RESTAURANT_Restaurant_ID` LIKE 1 ORDER BY RAND() LIMIT 1");
+$stmt2 = $pdo->query ("SELECT * FROM RESTAURANT ORDER BY RAND() LIMIT 1;");
+
+      
+$restaurants = [];
+while( $row = $stmt2->fetch() ) {
+    $restaurants[] = ['id' => $row['Restaurant_ID'],
+                      'name' => $row['Naam']
+                     ];
+}
+    
+$stmt = $pdo->query ("SELECT * FROM RECEPT WHERE `RESTAURANT_Restaurant_ID` LIKE {$restaurants[0]['id']} ORDER BY RAND() LIMIT 1");
+
+$recipes = [];
+
+while( $row = $stmt->fetch() ) {
+    $recipes[] = ['recipe_id' => $row['Recept_ID'],
+                      'name' => $row['Omschrijving'],
+                      'ingredients' => $row['Ingredienten'],
+                      'portions' => $row['Personen'],
+                        'cooking_time' => $row['Beredingstijd'],
+                      'restaurant_id' => $row['RESTAURANT_Restaurant_ID'],
+				  		'URL' => $row['URL']
+                     ];
+}
+
+
+      header( 'Content-type: application/json');
+
+      echo json_encode( $recipes );
+	  // echo json_encode( $restaurants );
+?>

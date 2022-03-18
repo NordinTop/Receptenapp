d606de796f0c4c3ad5dfd575cb8e5363f83d370e
NordinTop <78902281+NordinTop@users.noreply.github.com>
2022-03-18 09:08:22 +0100

    cicken

--- main.php
+++ main.php
@@ -39,7 +39,8 @@
     </nav>
     <div id="wrapper">
       <img src="images/wrapper.jpg" alt=""/>
-      <div id="thingy">&nbsp;</div>
+      <div id="thingy">&nbsp;
+      </div>
     </div>
   </div>
     
@@ -57,7 +58,7 @@
               echo '<h1 class="centered">Gerecht: '. $gerecht .'</h1>
               <div class="row">
                 <div class="col-3">
-                  <img src="images/steak.jpg" class="img-fluid">
+                  <img src="images/chicken.jpg" class="img-fluid">
                 </div>
                 <div class="col-9">
                   <p class="fw-bold">Ingrediënten: ' . $ingredient . '</p>
@@ -70,7 +71,7 @@
           <i class="fa-solid fa-arrow-rotate-right"></i> Nieuw recept
         </button>
       </div>
-      </div>
+    </div>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
   </body>
